4eacf6afa835d4ea64d83a5eb7c2f1943500e268
IT21340796 <it21340796@my.sliit.lk>
2023-05-02 07:57:47 +0530

    final project

--- app/src/main/java/com/example/cashreturningsystem/MainActivity.kt
+++ app/src/main/java/com/example/cashreturningsystem/MainActivity.kt
@@ -2,14 +2,31 @@ package com.example.cashreturningsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.cashreturningsystem.databinding.ActivityCreateBinding
+import com.example.cashreturningsystem.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.sql.DatabaseMetaData
 
 class MainActivity:AppCompatActivity() {
 
+private lateinit var binding: ActivityCreateBinding
+private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityCreateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.createBtn2.setOnClickListener{
+            val fullName = binding.fullName.text.toString()
+            val email = binding.email.text.toString()
+            val phone = binding.phone.text.toString()
+
+            database = FirebaseDatabase.getInstance().getReference("CashReturn")
+
+        }
 
 
     }
