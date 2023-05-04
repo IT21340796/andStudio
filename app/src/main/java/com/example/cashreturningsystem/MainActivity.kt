c7b465e8ddd6fbad5594cb935e1a721c110eac0f
IT21340796 <it21340796@my.sliit.lk>
2023-05-04 21:45:26 +0530

    branch commit

--- app/src/main/java/com/example/cashreturningsystem/MainActivity.kt
+++ app/src/main/java/com/example/cashreturningsystem/MainActivity.kt
@@ -1,31 +1,57 @@
 package com.example.cashreturningsystem
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.Button
-import com.example.cashreturningsystem.databinding.ActivityCreateBinding
-import com.example.cashreturningsystem.databinding.ActivityMainBinding
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cashreturningsystem.databinding.ActivityCreateReturnBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import java.sql.DatabaseMetaData
 
 class MainActivity:AppCompatActivity() {
 
-private lateinit var btnCreateData: Button
-private lateinit var btnFetchData: Button
-
+    private lateinit var binding: ActivityCreateReturnBinding
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create)
+        //setContentView(R.layout.activity_create)
 
-        btnFetchData = findViewById(R.id.cancelBtn2)
+        binding = ActivityCreateReturnBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.cancelBtn2.setOnClickListener {
 
-        btnFetchData.setOnClickListener {
+            val i = Intent(this@MainActivity, viewAllReturns::class.java)
+            startActivity(i)
+        }
 
+        binding.payBtn1.setOnClickListener {
+            val fName = binding.fullName.text.toString()
+            val email = binding.email.text.toString()
+            val phone = binding.phone.text.toString()
+            val cash = binding.cash.text.toString()
+            val csNIC = binding.NIC.text.toString()
+            val dateToCollect = binding.dateCollect.text.toString()
+
+            database = FirebaseDatabase.getInstance().getReference("CashReturn")
+
+            val cReturn = CashReturnModel(fName, email, phone, cash,csNIC,dateToCollect)
+            database.child(fName).setValue(cReturn).addOnSuccessListener {
+                binding.fullName.text?.clear()
+                binding.email.text?.clear()
+                binding.phone.text?.clear()
+                binding.cash.text?.clear()
+                binding.NIC.text?.clear()
+                binding.dateCollect.text?.clear()
+
+                Toast.makeText(this, "Successfuly Saved", Toast.LENGTH_LONG).show()
+
+                val i = Intent(this@MainActivity, viewAllReturns::class.java)
+                startActivity(i)
+            }.addOnFailureListener {
+                Toast.makeText(applicationContext, "Failed", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
